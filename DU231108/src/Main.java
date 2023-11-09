67c649ad496dec3968c6a64eae075e6fb7144f55
Milan Tuček <105244731+ClydeEmtea@users.noreply.github.com>
2023-11-09 10:30:06 +0100

    hotovson

--- DU231108/src/Main.java
+++ DU231108/src/Main.java
@@ -1,21 +1,23 @@
 import cz.gyarab.util.paint.*;
+import cz.gyarab.util.paint.Shape;
 
 import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        Painter painter = new Painter("Painter");
+        Painter painter = new Painter("Malíř");
 
-        Hexagon hexagon = new Hexagon(Color.RED, 50, 150, 50);
-        Pentagon pentagon = new Pentagon(Color.BLUE, 100, 50, 250);
-        Oktagon oktagon = new Oktagon(Color.GREEN, 50, 250, 250);
-        Trojuhelnik trojuhelnik = new Trojuhelnik(Color.YELLOW, 50, 150, 150);
-        Ctverec ctverec = new Ctverec(Color.BLACK, 50, 50, 50);
+        Shape hexagon = new Hexagon(Color.RED, 50, 150, 50);
+        Shape pentagon = new Pentagon(Color.BLUE, 100, 50, 250);
+        Shape oktagon = new Oktagon(Color.GREEN, 50, 250, 250);
+        Shape trojuhelnik = new Trojuhelnik(Color.YELLOW, 50, 150, 150);
+        Shape obdelnik = new Obdelnik(Color.BLACK, 50, 50, 50);
+
         painter.add(hexagon);
         painter.add(pentagon);
         painter.add(oktagon);
         painter.add(trojuhelnik);
-        painter.add(ctverec);
+        painter.add(obdelnik);
 
         painter.paint();
     }
--- DU231108/src/Obdelnik.java
+++ DU231108/src/Obdelnik.java
@@ -2,24 +2,28 @@ import cz.gyarab.util.paint.Shape;
 
 import java.awt.*;
 
-public class Ctverec extends Shape {
+public class Obdelnik extends Shape {
 
     private int size = 0;
     private final int[] positionsX = new int[4];
     private final int[] positionsY = new int[4];
 
-    public Ctverec(Color color, int size, int x, int y) {
+    public Obdelnik(Color color, int size, int x, int y) {
         super(color, x, y);
         this.size = size;
         setPositions();
     }
 
     private void setPositions() {
-        double angle = 360.0 / 4;
-        for (int i = 0; i < 4; i++) {
-            positionsX[i] = getX() + (int) (size * Math.cos(Math.toRadians(angle * i - 90)));
-            positionsY[i] = getY() + (int) (size * Math.sin(Math.toRadians(angle * i - 90)));
-        }
+        positionsX[0] = getX() - size;
+        positionsY[0] = getY() - size / 2;
+        positionsX[1] = getX() + size / 2;
+        positionsY[1] = getY() - size / 2;
+        positionsX[2] = getX() + size / 2;
+        positionsY[2] = getY() + size / 2;
+        positionsX[3] = getX() - size;
+        positionsY[3] = getY() + size / 2;
+
     }
 
     public void setSize(int size) {
